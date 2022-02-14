55fd3c1b5aa67f96881b164df0d72203ce917a48
aamir <engr.aamirkhan@gmail.com>
2022-02-14 12:57:23 +0000

    Updates

---
+++ precache-manifest.e6e732a1e51c6a9e8cc0c2b7a0f427ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8831fd89d39aa02b644bd1cc99b6bb6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03967f5950777c65fe3e",
+    "url": "/static/js/2.0b160038.chunk.js"
+  },
+  {
+    "revision": "b59861b9ca6e7b8651484dd212de5308",
+    "url": "/static/js/2.0b160038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213567b4adca9b3e3581",
+    "url": "/static/js/main.560f6de0.chunk.js"
+  },
+  {
+    "revision": "343ac363331221be2eaa",
+    "url": "/static/js/runtime-main.1f53e76c.js"
+  }
+]);
